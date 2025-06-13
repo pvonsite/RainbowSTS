a6f615fa54c6ecc4476cd7c3e90206880cbf349c
hbinhng <me@hbinhng.dev>
2025-06-13 21:40:09 +0000

    feat(ui/transcribe): Push transcribe output to view via events

--- static/js/transcribe.js
+++ static/js/transcribe.js
@@ -1,41 +1,46 @@
 class TranscribeView {
-    constructor(websocketHandler) {
-        this.websocketHandler = websocketHandler;
-        this.view = document.getElementById('original-text');
+  constructor(websocketHandler) {
+    this.websocketHandler = websocketHandler;
 
-        // Bind event listeners
-        this.websocketHandler.addEventListener('realtime', (data) => this.handleTranscription(data));
-        this.websocketHandler.addEventListener('fullSentence', (data) => this.finalizeSentence(data));
-        this.websocketHandler.addEventListener('connection', (data) => this.handleConnection(data));
-        this.fullSentences = '';
-        this.buildingSentence = ''
-    }
+    // Bind event listeners
+    this.websocketHandler.addEventListener("realtime", (data) =>
+      this.handleTranscription(data),
+    );
+    this.websocketHandler.addEventListener("fullSentence", (data) =>
+      this.finalizeSentence(data),
+    );
+    this.websocketHandler.addEventListener("connection", (data) =>
+      this.handleConnection(data),
+    );
+    this.fullSentences = "";
+    this.buildingSentence = "";
+  }
 
-    handleConnection(data) {
-        if (data && data.status === 'connected') {
-            console.log('WebSocket connected:', data);
-            this.view.textContent = '';
-        }
+  handleConnection(data) {
+    if (data && data.status === "connected") {
+      console.log("WebSocket connected:", data);
     }
+  }
 
-    handleTranscription(data) {
-        if (data && data.text) {
-            this.buildingSentence = data.text;
-            this.render()
-        }
+  handleTranscription(data) {
+    console.log("Transcription data received:", data);
+    if (data && data.text) {
+      this.buildingSentence = data.text;
+      state.pushLiveToken(data.text);
     }
+  }
 
-    finalizeSentence(data) {
-        console.log('Full sentence received:', data);
-        if (data && data.text) {
-            this.fullSentences = this.fullSentences.concat(data.text);
-            this.buildingSentence = '';
-            this.render()
-        }
+  finalizeSentence(data) {
+    console.log("Full sentence received:", data);
+    if (data && data.text) {
+      this.fullSentences += data.text;
+      this.buildingSentence = "";
+      this.render();
     }
+  }
 
-    render() {
-        this.view.textContent = this.fullSentences.concat(' ', this.buildingSentence).concat('...');
-        console.log(this.view.textContent)
-    }
+  render() {
+    this.fullSentences.concat(" ", this.buildingSentence).concat("...");
+  }
 }
+
